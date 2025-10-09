8e968dee2d1436dc8976ec6edfc8b60619a431dd
RandithaK <bharkula3@gmail.com>
2025-10-10 02:58:03 +0530

    feat: Implement AdminConfigController with endpoints for managing service types

--- admin-service/src/main/java/com/techtorque/admin_service/controller/AdminConfigController.java
+++ admin-service/src/main/java/com/techtorque/admin_service/controller/AdminConfigController.java
@@ -1,3 +1,28 @@
 package com.techtorque.admin_service.controller;
 
-public class AdminConfigController {}
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/service-types")
+@Tag(name = "Admin: Service Configuration", description = "Endpoints for managing available service types.")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminConfigController {
+
+  // @Autowired
+  // private ServiceTypeService serviceTypeService;
+
+  @GetMapping
+  public ResponseEntity<?> listServiceTypes() {
+    // TODO: Delegate to a local service that uses ServiceTypeRepository to fetch all types.
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping
+  public ResponseEntity<?> addServiceType(/* @RequestBody ServiceTypeDto dto */) {
+    // TODO: Delegate to a local service to create a new ServiceType.
+    return ResponseEntity.ok().build();
+  }
+}

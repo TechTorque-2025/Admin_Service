f0f0daa054168ea077431bc56ea43d73e4618139
RandithaK <bharkula3@gmail.com>
2025-10-10 02:57:32 +0530

    feat: Create ServiceTypeRepository interface for service type management

---
+++ admin-service/src/main/java/com/techtorque/admin_service/repository/ServiceTypeRepository.java
@@ -0,0 +1,9 @@
+package com.techtorque.admin_service.repository;
+
+import com.techtorque.admin_service.entity.ServiceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceTypeRepository extends JpaRepository<ServiceType, String> {
+}

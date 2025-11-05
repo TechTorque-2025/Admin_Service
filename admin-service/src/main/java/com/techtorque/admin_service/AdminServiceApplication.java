de563d1151b0a462a893687f8de8e29d0d534aa6
RandithaK <bharkula3@gmail.com>
2025-11-05 21:31:53 +0530

    refactor: Update main application class

--- admin-service/src/main/java/com/techtorque/admin_service/AdminServiceApplication.java
+++ admin-service/src/main/java/com/techtorque/admin_service/AdminServiceApplication.java
@@ -3,8 +3,10 @@ package com.techtorque.admin_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableJpaAuditing
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
